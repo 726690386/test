f12ad0c58c199f4c9ee9b15135014a07c5978e0d
zhuoyi <726690386@qq.com>
2020-10-15 19:50:01 +0800

    2020/10/15 19:49

--- a.cpp
+++ a.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main()
 {
-    int a,b,c;
+    int a,b;
     a = 1;
     b = 2;
     cout<<"hello world!"<<endl;
+    cout << a << b << endl;
     return 0;
 }
